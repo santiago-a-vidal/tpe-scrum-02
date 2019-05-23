69e9ae44b1661862ea69653ca08b0fe932f3bf49
NinuEnci <42392489+NinaEncinosa@users.noreply.github.com>
2019-05-23 11:28:35 -0300

    SCRUM0219-31 Comentado codigo de registro

--- MVC/model/RegistroModel.php
+++ MVC/model/RegistroModel.php
@@ -19,7 +19,7 @@ class RegistroModel extends Model {
         return $sentencia->fetch(PDO::FETCH_ASSOC);
       }
 
-    //Traer solo el id de usuario  
+    //Traer solo el id de usuario   
     function getUsuarioID($usuario)
        {
         $sentencia = $this->db->prepare( "SELECT id_usuario FROM usuario WHERE nombre = ? LIMIT 1");

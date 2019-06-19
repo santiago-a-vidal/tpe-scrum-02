398e2782d5e4e5c4eeac9798c4a39f1598bc23aa
jcanneva <49138206+jcanneva@users.noreply.github.com>
2019-06-19 14:57:21 -0300

    Fix envio reporte

--- MVC/controller/ReporteController.php
+++ MVC/controller/ReporteController.php
@@ -10,18 +10,19 @@ require_once('UsuarioController.php');
     function __construct()
       {
 		    parent::__construct();
+        $this->view = new NavegacionView();
         $this->model = new ReporteModel();
         $this->modelUsuario = new RegistroModel();
       }
 
 //Una vez completado el formulario de reporte, actualizo la BBDD con el nuevo reporte y muestro el numero de reporte al usuario
-    public function EnviarReporte()
+    public function enviarReporte()
       {
         try{
           $imagen=$_FILES['imagen']['tmp_name'];
           $id_usuario=$this->modelUsuario->getUsuarioID($_POST['usuario']);
-          if (!$id_usuario)
-              throw new Exception("Usuario no registrado");
+          if (!$_POST['latitud'])
+              throw new Exception("Marque un punto en el mapa");
           $this->model->storeReporte($id_usuario['id_usuario'],$_POST['latitud'],$_POST['longitud'],$imagen,$_POST['descripcion']);
           $codigo_reporte ="<script language='JavaScript'>alert('Codigo de Reporte: ".$this->model->lastInsertId()."')</script>";
 		      $this->Home($codigo_reporte);
@@ -32,7 +33,7 @@ require_once('UsuarioController.php');
           }
       }
 
-      public function BorrarReporte(){
+      public function borrarReporte(){
         $id_reporte = $_POST['id_reporte'];
         $this->model->borrarReporte($id_reporte);
         header('Location: '. MAPAADMIN);
